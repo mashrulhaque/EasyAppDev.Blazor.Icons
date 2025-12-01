be40e745930b322d075c9a971866a4800436f4fc
Mashrul Haque <mashrul@gmail.com>
2025-12-01 22:15:57 +0600

    bug fixes

--- src/EasyAppDev.Blazor.Icons/IconData/BootstrapIcons/BootstrapIcons.cs
+++
@@ -1,9 +0,0 @@
-namespace EasyAppDev.Blazor.Icons.Bootstrap;
-
-public static partial class BootstrapIcons
-{
-    public const string ViewBox = "0 0 16 16";
-    public const string Fill = "currentColor";
-
-    public static partial class Icons { }
-}
--- src/EasyAppDev.Blazor.Icons/IconData/MaterialDesignIcons/MaterialDesignIcons.cs
+++
@@ -1,9 +0,0 @@
-namespace EasyAppDev.Blazor.Icons.MaterialDesign;
-
-public static partial class MaterialDesignIcons
-{
-    public const string ViewBox = "0 0 24 24";
-    public const string Fill = "currentColor";
-
-    public static partial class Icons { }
-}
